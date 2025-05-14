6ab508cecb8febb6c101ae65c27f19a9bb2628a7
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2025-05-14 15:46:07 +0530

    size of structure expectation and real
    rather than 9 it become 12. it means its largest datatypes*number

---
+++ cpp/02d_structure.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+struct rectangle {
+    int lenght;
+    int breadth;
+    char string;
+} r1; 
+int main()
+{
+  r1={10,20};
+  std::cout<<sizeof(r1)<< std::endl;
+//   printf("%lu",sizeof(r1));
+}
